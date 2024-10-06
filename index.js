0505bc2500809b31b9687b29ca97cf0c19444b3e
Asfand322 <asfandsheikh321@gmail.com>
2024-10-06 22:00:03 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = +(Math.random())
+let b = +prompt("enter a number")
+if(b<a){
+    console.log("your number is smaller than real num")
+}
+else if(b>a){
+    console.log("your number is greater than real num")
+}
+else if(b=a){
+    console.log("congratulations you got it")
+}
