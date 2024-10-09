f182163bc6fc713eb7283f30c7661381c55877d9
Asfand322 <asfandsheikh321@gmail.com>
2024-10-09 12:07:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let a = +(Math.random())
-let b = +prompt("enter a number")
-if(b<a){
-    console.log("your number is smaller than real num")
-}
-else if(b>a){
-    console.log("your number is greater than real num")
-}
-else if(b=a){
-    console.log("congratulations you got it")
+alert("hi")
+let a = Math.random()*100;
+a = Number.parseInt(a)
+let inp;
+let score = 100
+while (inp != a) {
+    score = score - 1;
+    inp = prompt("enter a num");
+    if(inp == a){
+        console.log("congratulations you have a actual number")
+        console.log(`you have got in ${100 - score} chances`)
+    }
+    else if(inp > a && inp < 100){
+        console.log("your number is smaller than real num")
+    }
+    else if(inp < a && inp > 0){
+        console.log("your number is greater than real num")
+    }
+    else{
+        console.log("enter number inpetween 1 and 100")
+    }
 }
